5b48bbacee3f764b186d68c71803429e224bba3b
Francisco2020-Ra <franciscorafaelromero@gmail.com>
2022-10-18 18:59:00 -0300

    @Input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { EmpleadoComponent } from './empleado/empleado.component';
 import { FormsModule } from '@angular/forms';
+import { EmpleadoHijoCComponent } from './empleado-hijo-c/empleado-hijo-c.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmpleadoComponent
+    EmpleadoComponent,
+    EmpleadoHijoCComponent
   ],
   imports: [
     BrowserModule,FormsModule
